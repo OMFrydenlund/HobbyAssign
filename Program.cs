04fc0e8e8a0514fec7aca475b5c98a00fa99a691
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-15 14:56:59 +0100

    Hnnng...

--- Program.cs
+++ Program.cs
@@ -5,8 +5,13 @@ namespace HobbyAssign
     class Program
     {
         static void Main()
-        { 
-        
+        {
+            Console.WriteLine("*clown honks* Handing out some hobbies, who wants one *sigh*");
+            string nameInput = Console.ReadLine();
+
+            string[] hobbyArray = { "gamer", "diver", "clown", "randomHobby4", "randomHobby5" };
+
+            Random Rand = new Random();
         }
     }
 }
