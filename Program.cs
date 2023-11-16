40345e8a994e49cebaac3682d99841fdfec75f92
OMFrydenlund <migfrydenlund@gmail.com>
2023-11-16 14:03:46 +0100

    It works!

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-namespace HobbyAssign 
-{ 
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("*clown honks* Handing out some hobbies, who wants one *sigh*");
-            string nameInput = Console.ReadLine();
 
-            string[] hobbyArray = { "gamer", "diver", "clown", "randomHobby4", "randomHobby5" };
+        assignHobby();
+        void assignHobby()
+{
+    Console.WriteLine("*clown honks* Handing out some hobbies, who wants one *sigh*");
+    string nameInput = Console.ReadLine();
 
-            Random somethingRandom = new Random();
-        }
-    }
+    string[] hobbyArray = { "street lamp", "watercooler", "clown", "cup", "janitor" };
+
+    Random yourNewHobby = new Random();
+    int randomIndex = yourNewHobby.Next(hobbyArray.Length);
+
+    string randomHobby = hobbyArray[randomIndex];
+
+    Console.WriteLine($"Poof! {nameInput} is now a {randomHobby}!");
 }
